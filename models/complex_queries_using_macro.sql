d8c7034c9c3d34634111253f8f785d3408631696
Madhavi Chile <chilemadhavi1974@gmail.com>
2024-09-09 06:20:59 +0000

    09-09-2024 11:50am

--- models/complex_queries_using_macro.sql
+++ models/complex_queries_using_macro.sql
@@ -1,9 +1,10 @@
 select * 
 from {{source("flake1","Financials")}}
 
--- {% set currency_list = dbt_utils.get_filtered_columns_in_relation(source("flake1", "Financials"), except=["COUNTRY", "DISCOUNT"]) %}
-select 
-currency_converter(COUNTRY, SALES) as 'SALES_in_Rupees',
-currency_converter(COUNTRY, COST) as 'COST_in_Rupees'
-from {{ source("flake1", "Financials") }}
+{% set currency_list = dbt_utils.get_filtered_columns_in_relation(source("flake1", "Financials"), except=["COUNTRY", "DISCOUNT"]) %}
+select *,
+{% for i in currency_list %}
+    {{currency_converter('COUNTRY', i)}} as {{i}}_in_Rupees,
+{% endfor %}
+from {{ source('flake1', 'Financials') }}
 
